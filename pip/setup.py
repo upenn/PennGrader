90422349842a16f26b4a8d74674a4ed7e3cc3225
Zack Ives <zives@seas.upenn.edu>
2023-01-04 15:54:48 -0500

    Various merges

--- pip/setup.py
+++ pip/setup.py
@@ -2,17 +2,29 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
+<<<<<<< Updated upstream
      name='penn-grader',  
      version='0.5.0',
      scripts=[],
      author="Leonardo Murri and Zachary Ives",
+=======
+     name='penn_grader',  
+     version='0.5',
+     scripts=[],
+     author="Leonardo Murri and Zack Ives",
+>>>>>>> Stashed changes
      author_email="zives@cis.upenn.edu",
      description="In-line python grader client.",
      long_description=long_description,
    long_description_content_type="text/markdown",
+<<<<<<< Updated upstream
      url="https://github.com/zackives/PennGrader",
      packages=['penngrader'],
      install_requires=['dill','pyyaml'],
+=======
+     url="https://github.com/LeoMurri/PennGrader",
+     packages=['penn_grader'],
+>>>>>>> Stashed changes
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
